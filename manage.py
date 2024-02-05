3679208b8eb6adb7e72222c695ff098c9f575c27
Baanu <baanu.reddy@hotstar.com>
2024-02-05 11:09:28 +0530

    2

--- manage.py
+++ manage.py
@@ -24,8 +24,8 @@ if __name__ == "__main__":
 
     execute_from_command_line(sys.argv)
 #adding this line to check the commit
-username = "baanu"
-password = "password"
-api_key = "xobo-aaasdddmakakaldmdjdjd"
+username = "baanua"
+password = "passworda"
+api_key = "xobo-aaasdaddmakakaldmdjdjd"
 def hello:
-	print("baanu")
+	print("baanuaaaa")
